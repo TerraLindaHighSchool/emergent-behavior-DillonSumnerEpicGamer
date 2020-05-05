ef0cd50c97b149c86d89e941ee250e222e3c7343
DillonSumnerEpicGamer <0822djs@gmail.com>
2020-05-05 09:55:32 -0700

    Collecting Food Complete

--- ants/Ant.java
+++ ants/Ant.java
@@ -8,6 +8,10 @@ import greenfoot.*;
  */
 public class Ant extends Creature
 {
+    private boolean carryingFood;
+    private GreenfootImage image1 = getImage();
+    private GreenfootImage image2 = new GreenfootImage("ant-with-food.gif");
+    
     /**
      * Create an ant with a given home hill. The initial speed is zero (not moving).
      */
@@ -15,27 +19,68 @@ public class Ant extends Creature
     {
         setHomeHill(home);
     }
+    
+    public Ant()
+    {
+        carryingFood = false;
+    }
+    
+    private boolean atHome()
+    {
+        if (getHomeHill() != null) {
+            return (Math.abs(getX() - getHomeHill().getX()) < 4) && 
+                   (Math.abs(getY() - getHomeHill().getY()) < 4);
+        }
+        else {
+            return false;
+        }
+}
 
     /**
      * Do what an ant's gotta do.
      */
     public void act()
     {
-        randomWalk();
         
-        testFoodPile(); // This currently does not do anything
+        
+        status();
     }
     
-    private void testFoodPile()
+    private void checkForFood()
     {
     
     
         food food = (food) getOneIntersectingObject(food.class);
         if (food != null) 
         {
+            carryingFood = true;
+            setImage(image2);
             food.removeCrumb();
         }
     
     
+    }
+    
+    private void searchForFood()
+    {
+        randomWalk();
+        checkForFood();
+    }
+    
+    private void status()
+    {
+        if (carryingFood)
+        {
+            if (atHome())
+            {
+                setImage(image1);
+                carryingFood = false;
+                getHomeHill().countFood();
+            }
+        }
+        else
+        {
+           searchForFood(); 
+        }
     }
 }
--- ants/AntWorld.java
+++ ants/AntWorld.java
@@ -18,5 +18,40 @@ public class AntWorld extends World
     {
         super(SIZE, SIZE, 1);
         setPaintOrder(Ant.class, AntHill.class);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        AntHill antHill = new AntHill();
+        addObject(antHill,134,99);
+        AntHill antHill2 = new AntHill();
+        addObject(antHill2,528,549);
+        food food = new food();
+        addObject(food,484,100);
+        food food2 = new food();
+        addObject(food2,100,543);
+        food food3 = new food();
+        addObject(food3,341,298);
+        food food4 = new food();
+        addObject(food4,83,217);
+        food food5 = new food();
+        addObject(food5,312,37);
+        food food6 = new food();
+        addObject(food6,404,584);
+        food food7 = new food();
+        addObject(food7,553,415);
+        food food8 = new food();
+        addObject(food8,308,272);
+        food food9 = new food();
+        addObject(food9,304,328);
+        food food10 = new food();
+        addObject(food10,378,293);
+        food food11 = new food();
+        addObject(food11,357,336);
     }
 }
